ae32f8401846a5ebab0f3da6b65668a21346f2b8
Việt Hoàn <133645822+Kuwado@users.noreply.github.com>
2024-03-08 21:11:30 +0700

    Update gift.js

--- gift.js
+++ gift.js
@@ -1,7 +1,7 @@
 
 var to = 'Bạn tớ!';
 var gift_url = '';
-var gift_image_url = 'Pic/trg.jpg';
+var gift_image_url = 'https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/431025016_448261690890573_6759796660144100881_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGqQthqz_DBHxlXiJqogY-C_xJde7OBZo7_El17s4Fmjh7RcKuoFsGhVVTo-x_E2oNYUSD7xOJQuYXASRmTqDUl&_nc_ohc=xVdYNeWybVYAX8OFYhm&_nc_ht=scontent.fhan2-4.fna&oh=00_AfAHiLExoSGAeXsMthzUA_HC-VyDsiMVBCehIpe0GwEvSA&oe=65EF1284';
 
 
 
